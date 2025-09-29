c3b1e4934bbf0224e1c34f8b30edb6db7e6ac391
¨Alex <232560492+developeragencia-crypto@users.noreply.github.com>
2025-09-29 17:30:53 -0300

    fix: next.config.js ES module export e build validado

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
   images: {
     unoptimized: true,
     domains: ["ext.same-assets.com"],
@@ -20,5 +21,3 @@ const nextConfig = {
     '9dc07387-8c0c-487c-8aac-28642b565a24-00-3f0xxj7ddr2i4.kirk.replit.dev'
   ],
 };
-
-module.exports = nextConfig;
